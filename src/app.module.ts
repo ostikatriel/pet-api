3986eaadf614ea5f12494bdcf3e6db0337adcdd8
Osti Katriel <osti.katriel@gmail.com>
2025-11-05 23:00:30 -0500

    feat: creacion de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { PetsModule } from './pets/pets.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, PetsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
